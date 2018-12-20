58165e1f8acc2cc9d3fbe2598a6988a82ba003ea
brookylaris <46020366+brookylaris@users.noreply.github.com>
2018-12-20 17:37:05 +0000

    Update finduser.js

--- rage cmds/finduser.js
+++ rage cmds/finduser.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, message, args) => {
     let users = bot.users;
 
     let searchTerm = args[0];
-    if(!searchTerm) return message.channel.send("Please type a term to search!");
+    if(!searchTerm) return message.channel.send("Please type a Rage Member to search!");
 
     let matches = users.filter(u => u.tag.toLowerCase().includes(searchTerm.toLowerCase()));
     
