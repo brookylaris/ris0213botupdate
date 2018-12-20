47813e28b05706750fe2af7cf979aa74d8087063
brookylaris <46020366+brookylaris@users.noreply.github.com>
2018-12-20 17:44:43 +0000

    Update invite.js

--- rage cmds/invite.js
+++ rage cmds/invite.js
@@ -4,10 +4,9 @@ module.exports.run = async (bot, message, args) => {
   let bicon = bot.user.displayAvatarURL;
     
  let inviteEmbed = new Discord.RichEmbed()
- .setDescription("[**Invite**](http://bit.ly/OdarBot)")
+ .setDescription("[**Invite**](https://discord.gg/exEBTMA)")
  .setColor("#00ff00")
  .setThumbnail(bicon)
- .addField("Use this invite to invite the bot in your server!", "http://bit.ly/OdarBot")
 
  message.channel.send(inviteEmbed);
 
