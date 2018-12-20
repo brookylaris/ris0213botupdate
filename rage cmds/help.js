6389eefb9e31f804895b0dbb1cc943a453724c49
brookylaris <46020366+brookylaris@users.noreply.github.com>
2018-12-20 16:57:22 +0000

    Update help.js

--- rage cmds/help.js
+++ rage cmds/help.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
 
     const infoList = fs.readFileSync("./info.txt", "utf8");
-    const adminCommands = fs.readFileSync("./admin.txt", "utf8");
     const funcommands = fs.readFileSync("./fun.txt", "utf8");
     let bicon = bot.user.displayAvatarURL;
     const pidor = message.guild.members.get(args[0]) || message.member;
@@ -23,21 +22,13 @@ module.exports.run = async (bot, message, args) => {
 
     pidor.send(infoEmbed);
 
-    let modembed = new Discord.RichEmbed()
-    .setColor("00ff00")
-    .setTitle("**__Admin commands__**")
-    .setDescription(adminCommands)
-    
-    pidor.send(modembed);
 
     let supEmbed = new Discord.RichEmbed()
     .setColor("#00ff00")
     .setTitle("Support")
-    .setImage("https://cdn.discordapp.com/avatars/291221132256870400/4f71fea454b62405b55f2fe0d8e7db0c.png?size=2048")
-    .setDescription("If you need help with something feel free to join the [Support Server](https://discord.gg/zvvasbc)")
-    .setFooter("<> means required, [] means optional")
+    .setDescription("If you need help with something feel free to join the [Main Server](https://discord.gg/exEBTMA)")
+    .setFooter("Rage StatBot Created by:RageEmpress")
     .setTimestamp()
-    .addField("Contact", "To contact with the owner use `!contact`")
 
     pidor.send(supEmbed)
 
