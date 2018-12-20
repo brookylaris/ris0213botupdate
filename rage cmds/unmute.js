06d1b99b32e4f5e72da1244d487546e209cae0a0
brookylaris <46020366+brookylaris@users.noreply.github.com>
2018-12-20 18:03:01 +0000

    Update unmute.js

--- rage cmds/unmute.js
+++ rage cmds/unmute.js
@@ -1,15 +1,15 @@
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("You don't have the `Manage Messages` premission")
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("Larissa didn't give you `Manage Messages` premission")
 
         let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
         if(!toMute) return message.channel.sendMessage("Please mention an user or ID to mute!");
 
-        let role = message.guild.roles.find(r => r.name === "Odar Mute")
+        let role = message.guild.roles.find(r => r.name === "Rage Mute")
         
-        if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("This user is not muted!");
+        if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("This Rage Member is not muted!");
 
         await toMute.removeRole(role);
-        message.channel.sendMessage("The user has been unmuted!");
+        message.channel.sendMessage("The Rage Member has been unmuted!");
 
         message.delete();
 
